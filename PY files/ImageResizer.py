495821ce2bec3afbd291ae518cf7fe15123964e4
Nathan Winspear <n.winspear1@gmail.com>
2019-07-22 23:11:23 +1200

    Succesful Cropped Images
    Next work on pasting into a white background, cetred in 5:4 ratio

--- PY files/ImageResizer.py
+++ PY files/ImageResizer.py
@@ -7,16 +7,24 @@ image_1 = Image.open("../Photos/1.png")
 image_2 = Image.open("../Photos/2.png")
 image_3 = Image.open("../Photos/3.png")
 image_4 = Image.open("../Photos/4.png")
-image_5 = Image.open("../Photos/5.png")
 
-image_list = [image_1, image_2, image_3, image_4, image_5]
+image_list = [image_1, image_2, image_3, image_4]
 
 pixel_scanner = PixelScanner()
 
+def get_paste_box(edges):
+    left_x = edges[0].x
+    top_y = edges[1].y
+    right_x = edges[2].x
+    bottom_y = edges[3].y
+    paste_box = (left_x, top_y, right_x, bottom_y)
+    return paste_box
+
+
 for img in range(len(image_list)):
-    corners = pixel_scanner.get_corners(image_list[img])
-    crop_box = (corners[0].x, corners[0].y, corners[3].x, corners[3].y)
-    cropped_image = image_list[img].crop(crop_box)
+    edges = pixel_scanner.get_edges(image_list[img])
+    paste_box = get_paste_box(edges)
+    cropped_image = image_list[img].crop(paste_box)
     file_name = "{}.png".format(img)
-    cropped_image.save(file_name)
+    cropped_image.save("../Output/{}".format(file_name))
 
--- PY files/PixelScanner.py
+++ PY files/PixelScanner.py
@@ -10,6 +10,78 @@ class PixelScanner:
     def __init__(self):
         self.image = None
 
+    def contains_colour_range(self, lower, upper, pixel):
+        if pixel[0] in range(lower, upper) and pixel[1] in range(lower, upper) and pixel[2] in range(lower, upper):        
+            return True
+        elif pixel[0] in range(lower, upper) and pixel[1] in range(lower, upper) and pixel[2] in range(lower, upper):        
+            return True
+        elif pixel[0] in range(lower, upper) and pixel[1] in range(lower, upper) and pixel[2] in range(lower, upper):        
+            return True
+        elif pixel[0] in range(lower, upper) and pixel[1] in range(lower, upper) and pixel[2] in range(lower, upper):        
+            return True
+        else:
+            return False
+
+    def get_edges(self, image):
+        global image_width, image_height
+
+        self.image = image
+        image_width = list(self.image.size)[0]
+        image_height = list(self.image.size)[1]
+
+        edges = []
+
+        left_point = Point(0, (image_height / 2))
+        top_point = Point((image_width / 2), 0)
+        right_point = Point((image_width - 1), (image_height / 2))
+        bottom_point = Point((image_width / 2), (image_height - 1))
+
+        left_pixel = self.image.getpixel((left_point.x, left_point.y))
+        top_pixel = self.image.getpixel((top_point.x, top_point.y))
+        right_pixel = self.image.getpixel((right_point.x, right_point.y))
+        bottom_pixel = self.image.getpixel((bottom_point.x, bottom_point.y))
+
+        final_left_point = None
+        final_top_point = None
+        final_right_point = None
+        final_bottom_point = None
+
+        while final_left_point == None:
+            if self.contains_colour_range(0, 200, left_pixel):
+                final_left_point = Point((left_point.x - 2), left_point.y)
+                edges.append(final_left_point)
+            else:
+                left_point.shift(1, 0)
+                left_pixel = self.image.getpixel((left_point.x, left_point.y))
+
+        while final_top_point == None:
+            if self.contains_colour_range(0, 200, top_pixel):
+                final_top_point = Point(top_point.x, (top_point.y - 2))
+                edges.append(final_top_point)
+            else:
+                top_point.shift(0, 1)
+                top_pixel = self.image.getpixel((top_point.x, top_point.y))
+
+        while final_right_point == None:
+            if self.contains_colour_range(0, 200, right_pixel):
+                final_right_point = Point((right_point.x + 2), right_point.y)
+                edges.append(final_right_point)
+            else:
+                right_point.shift(-1, 0)
+                right_pixel = self.image.getpixel((right_point.x, right_point.y))
+
+        while final_bottom_point == None:
+            if self.contains_colour_range(0, 200, bottom_pixel):
+                final_bottom_point = Point(bottom_point.x, (bottom_point.y + 2))
+                edges.append(final_bottom_point)
+            else:
+                bottom_point.shift(0, -1)
+                bottom_pixel = self.image.getpixel((bottom_point.x, bottom_point.y))
+
+        return edges
+
+
+"""
     def get_corners(self, image):
         global image_width, image_height
 
@@ -22,20 +94,7 @@ class PixelScanner:
         corners.insert(1, self.get_top_right())
         corners.insert(2, self.get_bottom_left())
         corners.insert(3, self.get_bottom_right())
-        #print("5", str(self.TLPixel), str(self.TRPixel), str(self.BLPixel), str(self.BRPixel))
-        return corners
-
-    def contains_colour_range(self, lower, upper, TLPixel, TRPixel, BLPixel, BRPixel):
-        if TLPixel[0] in range(lower, upper) and TLPixel[1] in range(lower, upper) and TLPixel[2] in range(lower, upper):        
-            return True
-        elif TRPixel[0] in range(lower, upper) and TRPixel[1] in range(lower, upper) and TRPixel[2] in range(lower, upper):        
-            return True
-        elif BLPixel[0] in range(lower, upper) and BLPixel[1] in range(lower, upper) and BLPixel[2] in range(lower, upper):        
-            return True
-        elif BRPixel[0] in range(lower, upper) and BRPixel[1] in range(lower, upper) and BRPixel[2] in range(lower, upper):        
-            return True
-        else:
-            return False
+        return corners       
 
     def get_top_left(self):
 
@@ -131,8 +190,7 @@ class PixelScanner:
                 TRPixel = self.image.getpixel( (x, y - 1) )
                 BLPixel = self.image.getpixel( (x - 1, y) )
                 BRPixel = self.image.getpixel( (x, y) )
-
-
+"""
 
 
 
