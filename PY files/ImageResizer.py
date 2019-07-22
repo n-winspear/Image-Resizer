3349163ea33588709c68c4ee1de464124b75d644
Nathan Winspear <n.winspear1@gmail.com>
2019-07-22 20:06:57 +1200

    Crop Fixed
    Can now crop and save consistently, need to work out the crop area better

--- PY files/ImageResizer.py
+++ PY files/ImageResizer.py
@@ -11,20 +11,12 @@ image_5 = Image.open("../Photos/5.png")
 
 image_list = [image_1, image_2, image_3, image_4, image_5]
 
-def get_crop_box(corners):
-	crop_width = corners[0].x - corners[1].x
-	crop_height = corners[0].y - corners[3].y
-	crop_box = (corners[0].x, corners[0].y, crop_width, crop_height)
-	#print(crop_box)
-	return crop_box
-
 pixel_scanner = PixelScanner()
 
 for img in range(len(image_list)):
     corners = pixel_scanner.get_corners(image_list[img])
-    crop_box = get_crop_box(corners)
+    crop_box = (corners[0].x, corners[0].y, corners[3].x, corners[3].y)
     cropped_image = image_list[img].crop(crop_box)
     file_name = "{}.png".format(img)
-    #print(file_name + "Saved")
-    #cropped_image.save(file_name)
+    cropped_image.save(file_name)
 
--- PY files/PixelScanner.py
+++ PY files/PixelScanner.py
@@ -5,19 +5,10 @@ from Point import Point
 
 image_width = 0
 image_height = 0
-x = 0
-y = 0
 
 class PixelScanner:
     def __init__(self):
         self.image = None
-        self.TLPixel = None
-        self.TRPixel = None
-        self.BLPixel = None
-        self.BRPixel = None
-
-    def __repr__(self):
-        return ("PixelScanner Currently At: ({0}, {1})".format(x, y))
 
     def get_corners(self, image):
         global image_width, image_height
@@ -27,143 +18,119 @@ class PixelScanner:
         image_height = list(self.image.size)[1]
 
         corners = []
-        print("1", str(self.TLPixel), str(self.TRPixel), str(self.BLPixel), str(self.BRPixel))
         corners.insert(0, self.get_top_left())
-        print("2", str(self.TLPixel), str(self.TRPixel), str(self.BLPixel), str(self.BRPixel))
         corners.insert(1, self.get_top_right())
-        print("3", str(self.TLPixel), str(self.TRPixel), str(self.BLPixel), str(self.BRPixel))
         corners.insert(2, self.get_bottom_left())
-        print("4", str(self.TLPixel), str(self.TRPixel), str(self.BLPixel), str(self.BRPixel))
         corners.insert(3, self.get_bottom_right())
-        print("5", str(self.TLPixel), str(self.TRPixel), str(self.BLPixel), str(self.BRPixel))
+        #print("5", str(self.TLPixel), str(self.TRPixel), str(self.BLPixel), str(self.BRPixel))
         return corners
 
-    def contains_colour_range(self, lower, upper):
-        if self.TLPixel[0] in range(lower, upper) and self.TLPixel[1] in range(lower, upper) and self.TLPixel[2] in range(lower, upper):        
+    def contains_colour_range(self, lower, upper, TLPixel, TRPixel, BLPixel, BRPixel):
+        if TLPixel[0] in range(lower, upper) and TLPixel[1] in range(lower, upper) and TLPixel[2] in range(lower, upper):        
             return True
-        elif self.TRPixel[0] in range(lower, upper) and self.TRPixel[1] in range(lower, upper) and self.TRPixel[2] in range(lower, upper):        
+        elif TRPixel[0] in range(lower, upper) and TRPixel[1] in range(lower, upper) and TRPixel[2] in range(lower, upper):        
             return True
-        elif self.BLPixel[0] in range(lower, upper) and self.BLPixel[1] in range(lower, upper) and self.BLPixel[2] in range(lower, upper):        
+        elif BLPixel[0] in range(lower, upper) and BLPixel[1] in range(lower, upper) and BLPixel[2] in range(lower, upper):        
             return True
-        elif self.BRPixel[0] in range(lower, upper) and self.BRPixel[1] in range(lower, upper) and self.BRPixel[2] in range(lower, upper):        
+        elif BRPixel[0] in range(lower, upper) and BRPixel[1] in range(lower, upper) and BRPixel[2] in range(lower, upper):        
             return True
         else:
             return False
 
     def get_top_left(self):
-        global x, y, image_width, image_height
 
-        self.TLPixel = self.image.getpixel( (x, y) ) 
-        self.TRPixel = self.image.getpixel( (x + 1, y) )
-        self.BLPixel = self.image.getpixel( (x, y + 1) )
-        self.BRPixel = self.image.getpixel( (x + 1, y + 1) )
+        x = 0
+        y = 0
+
+        TLPixel = self.image.getpixel( (x, y) ) 
+        TRPixel = self.image.getpixel( (x + 1, y) )
+        BLPixel = self.image.getpixel( (x, y + 1) )
+        BRPixel = self.image.getpixel( (x + 1, y + 1) )
 
         for pixel_set in range(100):
-            if self.contains_colour_range(0, 200):
+            if self.contains_colour_range(0, 200, TLPixel, TRPixel, BLPixel, BRPixel):
                 x += 3
                 y += 3
                 return Point(x, y)
             else:
-                self.shift("DR")
+                x += 1
+                y += 1
+                TLPixel = self.image.getpixel( (x, y) ) 
+                TRPixel = self.image.getpixel( (x + 1, y) )
+                BLPixel = self.image.getpixel( (x, y + 1) )
+                BRPixel = self.image.getpixel( (x + 1, y + 1) )
 
     def get_top_right(self):
-        global x, y, image_width, image_height
+        global image_width, image_height
 
         x = image_width - 1
         y = 0
 
-        self.TLPixel = self.image.getpixel( (x - 1, y) ) 
-        self.TRPixel = self.image.getpixel( (x, y) )
-        self.BLPixel = self.image.getpixel( (x - 1, y + 1) )
-        self.BRPixel = self.image.getpixel( (x, y + 1) )
+        TLPixel = self.image.getpixel( (x - 1, y) ) 
+        TRPixel = self.image.getpixel( (x, y) )
+        BLPixel = self.image.getpixel( (x - 1, y + 1) )
+        BRPixel = self.image.getpixel( (x, y + 1) )
 
         for pixel_set in range(100):
-            if self.contains_colour_range(0, 200):
+            if self.contains_colour_range(0, 200, TLPixel, TRPixel, BLPixel, BRPixel):
                 x -= 3
                 y += 3
                 return Point(x, y)
             else:
-                self.shift("DL")
+                x -= 1
+                y += 1
+                TLPixel = self.image.getpixel( (x - 1, y) ) 
+                TRPixel = self.image.getpixel( (x, y) )
+                BLPixel = self.image.getpixel( (x - 1, y + 1) )
+                BRPixel = self.image.getpixel( (x, y + 1) )
 
     def get_bottom_left(self):
-        global x, y, image_width, image_height
+        global image_width, image_height
 
         x = 0
         y = image_height - 1
 
-        self.TLPixel = self.image.getpixel( (x, y - 1) ) 
-        self.TRPixel = self.image.getpixel( (x + 1, y - 1) )
-        self.BLPixel = self.image.getpixel( (x, y) )
-        self.BRPixel = self.image.getpixel( (x + 1, y) )
+        TLPixel = self.image.getpixel( (x, y - 1) ) 
+        TRPixel = self.image.getpixel( (x + 1, y - 1) )
+        BLPixel = self.image.getpixel( (x, y) )
+        BRPixel = self.image.getpixel( (x + 1, y) )
 
         for pixel_set in range(100):
-            if self.contains_colour_range(0, 200):
+            if self.contains_colour_range(0, 200, TLPixel, TRPixel, BLPixel, BRPixel):
                 x += 3
                 y -= 3
                 return Point(x, y)
             else:
-                self.shift("UR")
+                x += 1
+                y -= 1
+                TLPixel = self.image.getpixel( (x, y - 1) ) 
+                TRPixel = self.image.getpixel( (x + 1, y - 1) )
+                BLPixel = self.image.getpixel( (x, y) )
+                BRPixel = self.image.getpixel( (x + 1, y) )
 
     def get_bottom_right(self):
-        global x, y, image_width, image_height
+        global image_width, image_height
 
         x = image_width - 1
         y = image_height - 1
 
-        self.TLPixel = self.image.getpixel( (x - 1, y - 1) ) 
-        self.TRPixel = self.image.getpixel( (x, y - 1) )
-        self.BLPixel = self.image.getpixel( (x - 1, y) )
-        self.BRPixel = self.image.getpixel( (x, y) )
+        TLPixel = self.image.getpixel( (x - 1, y - 1) ) 
+        TRPixel = self.image.getpixel( (x, y - 1) )
+        BLPixel = self.image.getpixel( (x - 1, y) )
+        BRPixel = self.image.getpixel( (x, y) )
 
         for pixel_set in range(100):
-            if self.contains_colour_range(0, 200):
+            if self.contains_colour_range(0, 200, TLPixel, TRPixel, BLPixel, BRPixel):
                 x -= 3
                 y -= 3
                 return Point(x, y)
             else:
-                self.shift("UL")
-
-    def shift(self, shift_direction):
-        global x, y, image_width, image_height
-
-        if shift_direction == "DR":
-            x += 1
-            y += 1
-            self.TLPixel = self.image.getpixel( (x, y) ) 
-            self.TRPixel = self.image.getpixel( (x + 1, y) )
-            self.BLPixel = self.image.getpixel( (x, y + 1) )
-            self.BRPixel = self.image.getpixel( (x + 1, y + 1) )
-            return    
-
-        elif shift_direction == "DL":
-            x -= 1
-            y += 1
-            self.TLPixel = self.image.getpixel( (x - 1, y) ) 
-            self.TRPixel = self.image.getpixel( (x, y) )
-            self.BLPixel = self.image.getpixel( (x - 1, y + 1) )
-            self.BRPixel = self.image.getpixel( (x, y + 1) )
-            return
-
-        elif shift_direction == "UR":
-            x += 1
-            y -= 1
-            self.TLPixel = self.image.getpixel( (x, y - 1) ) 
-            self.TRPixel = self.image.getpixel( (x + 1, y - 1) )
-            self.BLPixel = self.image.getpixel( (x, y) )
-            self.BRPixel = self.image.getpixel( (x + 1, y) )
-            return
-
-        elif shift_direction == "UL":
-            x -= 1
-            y -= 1
-            self.TLPixel = self.image.getpixel( (x - 1, y - 1) ) 
-            self.TRPixel = self.image.getpixel( (x, y - 1) )
-            self.BLPixel = self.image.getpixel( (x - 1, y) )
-            self.BRPixel = self.image.getpixel( (x, y) )
-            return
-            
-
-        
+                x -= 1
+                y -= 1
+                TLPixel = self.image.getpixel( (x - 1, y - 1) ) 
+                TRPixel = self.image.getpixel( (x, y - 1) )
+                BLPixel = self.image.getpixel( (x - 1, y) )
+                BRPixel = self.image.getpixel( (x, y) )
 
 
 
