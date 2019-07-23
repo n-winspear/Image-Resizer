ce2c842bc4fafd4b9f24782ac887e84a6b78c550
n-winspear <n.winspear1@gmail.com>
2019-07-23 21:49:57 +1200

    Project Completed!
    The code was completed, files are now imported and the code runs without error.
    
    PROJECT COMPLETED!!!

--- PY files/ImageResizer.py
+++ PY files/ImageResizer.py
@@ -2,23 +2,15 @@
 
 from PIL import Image
 from PixelScanner import PixelScanner
-
-image_1 = Image.open("../Photos/1.png")
-image_2 = Image.open("../Photos/2.png")
-image_3 = Image.open("../Photos/3.png")
-image_4 = Image.open("../Photos/4.png")
-
-image_list = [image_1, image_2, image_3, image_4]
-
-pixel_scanner = PixelScanner()
+import os
 
 def get_paste_box(edges):
-    left_x = edges[0].x
-    top_y = edges[1].y
-    right_x = edges[2].x
-    bottom_y = edges[3].y
-    paste_box = (left_x, top_y, right_x, bottom_y)
-    return paste_box
+		    left_x = edges[0].x
+		    top_y = edges[1].y
+		    right_x = edges[2].x
+		    bottom_y = edges[3].y
+		    paste_box = (left_x, top_y, right_x, bottom_y)
+		    return paste_box
 
 def add_background(cropped_image):
     max_image_size = (400, 500)
@@ -28,11 +20,16 @@ def add_background(cropped_image):
     background.paste(cropped_image, offset)
     return background
 
-for img in range(len(image_list)):
-    edges = pixel_scanner.get_edges(image_list[img])
-    paste_box = get_paste_box(edges)
-    cropped_image = image_list[img].crop(paste_box)
-    final_image = add_background(cropped_image)
-    file_name = "{}.png".format(img)
-    final_image.save("../Output/{}".format(file_name))
-
+for folder in os.listdir("C:/Users/nwins/Documents/GitHub/Image-Resizer/Originals"):
+	folder_name = str(folder)
+	counter = 1
+	pixel_scanner = PixelScanner()
+	for image in os.listdir("C:/Users/nwins/Documents/GitHub/Image-Resizer/Originals/{}".format(folder)):
+		working_image = Image.open("C:/Users/nwins/Documents/GitHub/Image-Resizer/Originals/{}/{}".format(folder_name, image))
+		edges = pixel_scanner.get_edges(working_image)
+		paste_box = get_paste_box(edges)
+		cropped_image = working_image.crop(paste_box)
+		final_image = add_background(cropped_image)
+		file_name = "{}_{}.png".format(folder_name, counter)
+		final_image.save("C:/Users/nwins/Documents/GitHub/Image-Resizer/Processed/{}/{}".format(folder_name, file_name))
+		counter += 1
